75a9e921bda791f91a67a6758846a142bdf5daa6
al3xst <al3xst@users.noreply.github.com>
2017-05-29 00:08:16 +0200

    Update .appveyor_build.sh

--- .appveyor_build.sh
+++ .appveyor_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo `git branch | grep \* | cut -d ' ' -f2`
+echo `git rev-parse --abbrev-ref HEAD`
 
 pacman -Syu --noconfirm
 pacman --sync --noconfirm mingw-w64-x86_64-toolchain # msys64-only
