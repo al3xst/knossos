e630a4558c40e479a6ca7d1cee3d1b5006516f70
al3xst <al3xst@users.noreply.github.com>
2017-06-19 23:22:57 +0200

    Test qt5.8.0 download from netcologne server

--- travis/installqt5.sh
+++ travis/installqt5.sh
@@ -4,7 +4,8 @@ echo Downloading Qt
 set -v
 set -e
 
-DOWNLOAD_URL=https://download.qt.io/archive/qt/5.8/5.8.0/qt-opensource-mac-x64-clang-5.8.0.dmg
+#DOWNLOAD_URL=https://download.qt.io/archive/qt/5.8/5.8.0/qt-opensource-mac-x64-clang-5.8.0.dmg
+DOWNLOAD_URL=http://mirror.netcologne.de/qtproject/archive/qt/5.8/5.8.0/qt-opensource-mac-x64-clang-5.8.0.dmg
 INSTALLER=`basename $DOWNLOAD_URL`
 INSTALLER_NAME=${INSTALLER%.*}
 ENVFILE=qt-5.8.0-osx.env
